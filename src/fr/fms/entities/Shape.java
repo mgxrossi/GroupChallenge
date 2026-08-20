2f85d9a121b635b714df164669a26dcb3f9ec679
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-08-20 13:12:01 +0200

    Ajout des entities

--- src/fr/fms/entities/Shape.java
+++ src/fr/fms/entities/Shape.java
@@ -1,5 +1,30 @@
 package fr.fms.entities;
 
-public class Shape {
+public abstract class Shape {
+    private Point center;
 
+    public Shape(int x, int y) {
+        this.center = new Point(x, y);
+    }
+
+    public Shape(Point center) {
+        if (center != null) {
+            this.center = new Point(center.getX(), center.getY());
+        }
+    }
+
+    public abstract double area();
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public void setCenter(Point center) {
+        this.center = center;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape [center = " + center + "]";
+    }
 }
